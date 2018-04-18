9adc197485b44edecf10affd561a1ee6dc6957ec
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 21:16:29 +0200

    zkraceni a pridani input

--- 13. matice.py
+++ 13. matice.py
@@ -1,25 +1,18 @@
+pocet_radku = int(input('Zadej počt řádků:'))
 pismeno = 'X'
 mezera = ' '
 jednotka = pismeno + mezera
 
-
-for cislo_radku in range(0,6):
-    if cislo_radku == 0:
-        for opakovani in range(0,6):
+for cislo_radku in range(0,pocet_radku+1):
+    if cislo_radku == 0 or cislo_radku == pocet_radku:
+        for opakovani in range(0,pocet_radku+1):
             print(jednotka, end = '')
         print('\n')
-    elif cislo_radku == 5:
-        for opakovani in range(0,6):
-            print(jednotka, end='')
-        print('\n')
     else:
-        for opakovani in range(0,6):
-            if opakovani == 0:
-                print(jednotka, end='')
-            elif opakovani == 5:
+        for opakovani in range(0,pocet_radku+1):
+            if opakovani == 0 or opakovani == pocet_radku:
                 print(jednotka, end='')
             else:
                 print(mezera*2, end='')
         print('\n')
     cislo_radku = cislo_radku+1
-
