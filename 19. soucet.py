ef60d3de6ff43f532ffc44dfe85d152348cef7ed
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 21:26:22 +0200

    Create 19. soucet.py

---
+++ 19. soucet.py
@@ -0,0 +1,10 @@
+a = int(input('Zadej první číslo:'))
+b = int(input('Zadej druhé číslo:'))
+c = int(input('Zadej třetí číslo:'))
+
+soucet = a + b + c
+
+if soucet <10:
+    print('Součet je menší než 10')
+else:
+    print('Součet je větší než 10')
