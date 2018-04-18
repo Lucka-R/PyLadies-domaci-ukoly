e1dd897db67c5a01ab3764e4a23265b81608481a
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 22:14:03 +0200

    Update 22. faktorial.py

--- 22. faktorial.py
+++ 22. faktorial.py
@@ -1,7 +1,12 @@
 cislo = int(input('Zadej číslo:'))
 faktorial = 1
 
-for cislo in range (1,cislo+1):
-    faktorial = faktorial * cislo
-    cislo = cislo + 1
+if cislo < 0:
+    print ('Faktoriál záporného čísla neexistuje')
+if cislo == 0:
+    print ('Faktoriál nuly je 1')
+else:
+    for cislo in range (1,cislo+1):
+        faktorial = faktorial * cislo
+        cislo = cislo + 1
     print (faktorial)
