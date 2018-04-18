a8c6c7dd5bd73e53e4629a33973ba6f99db604e4
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 15:39:01 +0200

    první odstranění duplicitního kódu

--- 13. matice.py
+++ 13. matice.py
@@ -1,22 +1,25 @@
 pismeno = 'X'
 mezera = ' '
+jednotka = pismeno + mezera
+
 
 for cislo_radku in range(0,6):
     if cislo_radku == 0:
         for opakovani in range(0,6):
-            print(pismeno, mezera, end='')
+            print(jednotka, end = '')
         print('\n')
     elif cislo_radku == 5:
         for opakovani in range(0,6):
-            print(pismeno, mezera, end='')
+            print(jednotka, end='')
         print('\n')
     else:
         for opakovani in range(0,6):
             if opakovani == 0:
-                print(pismeno, mezera, end='')
+                print(jednotka, end='')
             elif opakovani == 5:
-                print(pismeno, mezera, end='')
+                print(jednotka, end='')
             else:
-                print(mezera, mezera, end='')
+                print(mezera*2, end='')
         print('\n')
     cislo_radku = cislo_radku+1
+
