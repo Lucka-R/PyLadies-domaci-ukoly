31ded96254f2bd4687fc3b0401cb437156289453
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 14:32:37 +0200

    Create 10. nasobilka.py

---
+++ 10. nasobilka.py
@@ -0,0 +1,7 @@
+for cislo_radku in range(0,5):
+    cislo = 0
+    for opakovani in range(0,5):
+        print (cislo, end=' ')
+        cislo=cislo+cislo_radku
+    print('\n')
+    cislo_radku=cislo_radku+1
