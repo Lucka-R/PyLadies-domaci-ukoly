7c194908196131874dde239f9f42c5b89c60b82c
Lucka-R <38294028+Lucka-R@users.noreply.github.com>
2018-04-18 14:39:02 +0200

    Update 09. xxx.py
    znovu opraveny mezery

--- 09. xxx.py
+++ 09. xxx.py
@@ -1,6 +1,6 @@
 for cislo_radku in range(0,5):
-    pismeno = 'X'
+    pismeno = 'X '
     for opakovani in range(0,5):
-        print (pismeno, end=' ')
+        print (pismeno, end='')
     print('\n')
     cislo_radku=cislo_radku + 1
